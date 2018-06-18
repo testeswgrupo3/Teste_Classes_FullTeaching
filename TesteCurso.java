fb30d44f56660e03765e71b3a338743fc23503b1
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-06-18 12:12:27 -0300

    aumetada a porcentegem de mutantes mortos

--- TesteCurso.java
+++ TesteCurso.java
@@ -5,7 +5,6 @@ public class TesteCurso {
   Course c;
   String imagem = "imagem";
   String nome = "nome";
-  User teacher;
   @Before
   public void before(){
     c = new Course(nome,imagem);
@@ -17,24 +16,19 @@ public class TesteCurso {
   }
 
   @Test
-  public void testaId(){
-
-    assertNotEquals("Id nulo",null,c.getId());
-    long id = 12321;
-    c.setId(id);
-    assertEquals("IDS DIFERENTES",id,c.getId());
-  }
-
-  @Test
   public void testaComparacao(){
-      assertEquals("Cursos iguas dando diferente",true,c.equals(c));
-      assertEquals("Cursos difentes dando iguais",false,c.equals(this));
+      Course c2 = new Course(nome,imagem);
+      c.setId(1);
+      c2.setId(2);
+      assertTrue("Cursos iguas dando diferente",c.equals(c));
+      assertFalse("Cursos difentes dando iguais",c.equals(c2));
   }
 
   @Test
   public void variaveisIniciais(){
-      assertEquals("Titulo do curso incorreto",c.getTitle(),nome);
-      assertEquals("Imagem do curso incorreta",c.getImage(),teacher);
+      Course c2 = new Course(nome,imagem);
+      assertEquals("Titulo do curso incorreto",c2.getTitle(),nome);
+      assertEquals("Imagem do curso incorreta",c2.getImage(),imagem);
 
   }
 
@@ -42,11 +36,29 @@ public class TesteCurso {
   public void testaSets(){
       String novoTitulo = "novo titulo";
       String novaImagen = "image2";
+      c.setId(1);
       c.setTitle(novoTitulo);
       c.setImage(novaImagen);
       assertEquals("Titulo do curso incorreto",c.getTitle(),novoTitulo);
       assertEquals("Imagem do curso incorreta",c.getImage(),novaImagen);
+      assertEquals("Id do curso incorreto",c.getId(),1);
+      assertEquals("Id do curso incorreto",c.getId(),1);
   }
 
+  @Test
+  public void testaToString(){
+    assertEquals("Função ToString Incorreta","Course[title: \"" + nome + "]",c.toString());
+  }
 
+  @Test
+  public void testaInstancias(){
+    Course c3 = new Course("3","3");
+    Course c4 = new Course("4","4");
+    c3.setId(3);
+    c4.setId(4);
+    assertNotEquals("Titulo diferentes de cursos diferentes dando iguais",c3.getTitle(),c4.getTitle());
+    assertNotEquals("Images diferentes de cursos diferentes dando iguais",c3.getImage(),c4.getImage());
+    assertNotEquals("IDs diferentes de cursos diferentes dando iguais",c3.getId(),c4.getId());
+
+  }
 }
