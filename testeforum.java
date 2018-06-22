674b3cc8f698ebbc4b408ed0ee876f23d7244678
testeswgrupo3 <38798704+testeswgrupo3@users.noreply.github.com>
2018-06-22 15:41:10 -0300

    Update testeforum.java
    adicionado prototipo testeforum

--- testeforum.java
+++ testeforum.java
@@ -17,8 +17,9 @@ public class testeforum {
   {
     assertNotEquals("Ativação",null,forum.getActivated().length());
   }
+
   @Test
-  public void testa()
+  public void testaForum()
   {
      Forum f1 = new Forum(activated);
      forum.setActivated("true");
@@ -28,49 +29,21 @@ public class testeforum {
   }
 
   @Test
-  public void testaForum()
-  {
-    Forum f1 = new Forum(activated);
-    assertEquals("Forum diferentes ativados ao mesmo tempo");
-    
-  }
+  public void testaInstancias(){
+    Forum f2 = new Forum("true","true");
+    Forum f3 = new Forum("falso","falso");
+    f2.setActivated("true");
+    f3.setActivated("falso");
+    assertEquals("Forum diferente ativados ao mesmo tempo",f2.getActivated(),f3.getActivated());
 
+  }
 
   @Test
   public void variaveisIniciais(){
-      Course c2 = new Course(nome,imagem);
-      assertEquals("Titulo do curso incorreto",c2.getTitle(),nome);
-      assertEquals("Imagem do curso incorreta",c2.getImage(),imagem);
+      Course f1 = new Forum(activated);
+      assertEquals("Ativação do forum incorreta", f1.getActivated(),activated);
 
-  }
 
-  @Test
-  public void testaSets(){
-      String novoTitulo = "novo titulo";
-      String novaImagen = "image2";
-      c.setId(1);
-      c.setTitle(novoTitulo);
-      c.setImage(novaImagen);
-      assertEquals("Titulo do curso incorreto",c.getTitle(),novoTitulo);
-      assertEquals("Imagem do curso incorreta",c.getImage(),novaImagen);
-      assertEquals("Id do curso incorreto",c.getId(),1);
-      assertEquals("Id do curso incorreto",c.getId(),1);
-  }
 
-  @Test
-  public void testaToString(){
-    assertEquals("Função ToString Incorreta","Course[title: \"" + nome + "]",c.toString());
-  }
 
-  @Test
-  public void testaInstancias(){
-    Course c3 = new Course("3","3");
-    Course c4 = new Course("4","4");
-    c3.setId(3);
-    c4.setId(4);
-    assertNotEquals("Titulo diferentes de cursos diferentes dando iguais",c3.getTitle(),c4.getTitle());
-    assertNotEquals("Images diferentes de cursos diferentes dando iguais",c3.getImage(),c4.getImage());
-    assertNotEquals("IDs diferentes de cursos diferentes dando iguais",c3.getId(),c4.getId());
-
-  }
 }
