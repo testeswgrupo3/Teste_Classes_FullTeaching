4d904dc4e7356b755ff94dd18b4bf453aff81a3f
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-07-08 11:29:07 -0300

    100% de mutantes mortos

--- TesteCourseDetails.java
+++ TesteCourseDetails.java
@@ -4,17 +4,32 @@ import org.junit.*;
 public class TesteCourseDetails {
 
   @Test
-  public void testaInfo(){
-    CourseDetails c  = new CourseDetails();
-    String info = "Informação do curso";
-    c.setInfo(info);
-    assertEquals("Informação do curso ","",c.getInfo());
+  public void testaConstrutores(){
+      CourseDetails cd = new CourseDetails();
+      assertEquals("Informação não está sendo inicializada com \"\"",cd.getInfo(),"");
   }
 
   @Test
-  public void variaveisIniciais(){
-    CourseDetails c  = new CourseDetails();
-    assertNotEquals("Informação inicial diferente de 'vazio'","",c.getInfo());
+  public void testaSets(){
+      CourseDetails cd = new CourseDetails();
+      cd.setId(10);
+      cd.setInfo("informação");
+      assertEquals("Id setado esta incorreto",cd.getId(),10);
+      assertEquals("Não foi possivel setar a informação",cd.getInfo(),"informação");
+  }
+
+  @Test
+  public void testaInstancias(){
+    CourseDetails cd = new CourseDetails();
+    cd.setId(10);
+    cd.setInfo("informação");
+
+    CourseDetails cd2 = new CourseDetails();
+    cd2.setId(20);
+    cd2.setInfo("outra informação");
+
+    assertNotEquals("Id's diferentes dando iguais",cd.getId(),cd2.getId());
+    assertNotEquals("Informaçẽos diferentes dando iguais",cd.getInfo(),cd2.getInfo());
 
   }
 
