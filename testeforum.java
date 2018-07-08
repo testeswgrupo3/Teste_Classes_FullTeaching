2abd811a7b6999cb8ac03d9b5f032e00caa10afa
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-07-08 11:16:46 -0300

    100% de mutantes mortos

--- testeforum.java
+++ testeforum.java
@@ -4,28 +4,40 @@ import org.junit.*;
 public class testeforum {
 
   @Test
-  public void testaAtivacaoForum()
-  {
-     Forum f1 = new Forum(false);
-     f1.setActivated(true);
-     assertTrue("Não foi possível ativar o forum", f1.isActivated());
+  public void testaConstrutores(){
+      Forum f = new Forum(true);
+      assertEquals("Não foi possivel cria o forum ativado",f.isActivated(),true);
+  }
+
+  @Test
+  public void testaSets(){
+      Forum f = new Forum(true);
+      f.setId(10);
+      f.setActivated(false);
+      assertEquals("Id setado esta incorreto",f.getId(),10);
+      assertEquals("Não foi possivel setar o forum como ativado",f.isActivated(),false);
+  }
+
+  @Test
+  public void testaToString(){
+    Forum f = new Forum(true);
+		String resultado = "Forum[activated: \"true\", #comments: \"0\"]";
+		assertEquals("funçao to toString incorreta",f.toString(),resultado);
 
   }
 
   @Test
-  public void testaMultiplosForums(){
+  public void testaInstancias(){
+    Forum f = new Forum(true);
+    f.setId(10);
+    f.setActivated(false);
+
     Forum f2 = new Forum(true);
-    Forum f3 = new Forum(true);
-    assertTrue("Forum não foi ativado",f2.isActivated());
-    assertTrue("Forum não foi ativado",f3.isActivated());
+    f2.setId(20);
+    f2.setActivated(true);
 
-  }
+    assertNotEquals("Id's diferentes dando iguais",f.getId(),f2.getId());
+    assertNotEquals("Estado de forum diferentes dando iguais",f.isActivated(),f2.isActivated());
 
-  @Test
-  public void variaveisIniciais(){
-      Forum f1 = new Forum(true);
-      assertEquals("Falha ao criar forum ativado",true, f1.isActivated());
-      Forum f2 = new Forum(false);
-      assertEquals("Falha ao criar forum desativado",false, f1.isActivated());
   }
 }
